4aac79782fd5f5528a7e0f683de51f139eeb3dbb
Hady Mohamed <hadymohamed@pursuit.org>
2021-07-08 02:30:20 -0400

    finished Show.js Pages Setup

--- tuner-api-frontend/src/Pages/Show.js
+++ tuner-api-frontend/src/Pages/Show.js
@@ -1,5 +1,19 @@
-import React from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import SongDetails from "../Components/SongDetails";
 
-export default function Show() {
-  return <div></div>;
+function Show({ songs, deleteSong }) {
+  let { index } = useParams();
+  const [song] = useState(songs[index]);
+
+  return (
+    <div className="Show">
+      <h1>📀 💿 Song Details 💿 📀</h1>
+      <section>
+        <SongDetails song={song} index={index} deleteSong={deleteSong} />
+      </section>
+    </div>
+  );
 }
+
+export default Show;
