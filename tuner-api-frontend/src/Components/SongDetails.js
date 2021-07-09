91bcbf86d9e16c1cf180c5bd69145619ae65ee0a
Hady Mohamed <hadymohamed@pursuit.org>
2021-07-09 13:20:34 -0400

    set up the logic for SongDetails logic need to set up the return logic

--- tuner-api-frontend/src/Components/SongDetails.js
+++ tuner-api-frontend/src/Components/SongDetails.js
@@ -1,5 +1,32 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Link, useParams, useHistory, withRouter } from "react-router-dom";
+import axios from "axios";
+import { apiURL } from "../util/apiURL";
 
-export default function SongDetails() {
+const API_BASE = apiURL();
+
+function SongDetails(props) {
+  const { deleteSong } = props;
+  const [song, setSong] = useState([]);
+
+  let { index } = useParams();
+  let history = useHistory();
+
+  useEffect(() => {
+    axios.get(`${API_BASE}/songs/${index}`).then((response) => {
+      const { data } = response;
+      setSong(data).catch((e) => {
+        history.push("/not-found");
+      });
+    });
+  }, [index, history]);
+
+  const handleDelete = () => {
+    deleteSong(index);
+    history.push("/songs");
+  };
+
   return <div></div>;
 }
+
+export default withRouter(SongDetails);
